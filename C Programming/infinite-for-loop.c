de6e4900f97c20afe06056495b6364d2249ea429
Homeless Ghetto Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-04 00:01:12 +0700

    Create infinite-for-loop.c

---
+++ C Programming/infinite-for-loop.c
@@ -0,0 +1,21 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+//infinite for loop
+
+int main(int argc, char **argv)
+{
+
+
+
+for(;;)
+{
+        printf("Infinite for loop!\n");
+}
+
+
+return 0;
+}
+
+
