4c548a221408b341cb94d83785f7256a4c8df356
Homeless Ghetto Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-03 17:16:45 +0700

    Create relational-operator.c

---
+++ C Programming/relational-operator.c
@@ -0,0 +1,53 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+//Relational operator
+
+int main(int argc, char **argv)
+{
+int a = 1;
+
+//Equal- relational operator
+if(a==1)
+{
+        printf("\"==\" a variable is equal 1\n");
+}
+
+//Not equal- relational operator
+if(a!=0)
+{
+        printf("\"!=\" a variable is not equal 0 \n");
+}
+
+//Greater than- relational operator
+if(a>0)
+{
+        printf("\">\" a variable is greater than 0\n");
+}
+
+//Less than- relational operator
+if(a<2)
+{
+        printf("\"<\" a variable is less than 2\n");
+}
+
+//Greater than equal- relational operator
+if(a<=1)
+{
+        printf("\"<=\" a variable is less than equal 1\n");
+}
+
+//Less than equal- relational operator 
+if(a>=1)
+{
+        printf("\">=\" a variable is greater than equal 1\n");
+}
+
+
+
+
+
+return 0;
+}
+                                                                                                                                    51,1          Bot
