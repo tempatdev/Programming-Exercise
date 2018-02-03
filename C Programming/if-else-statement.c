14a855bd1008a40b602226448aa871e1bc4c185d
Homeless Ghetto Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-03 23:00:32 +0700

    Create if-else-statement.c

---
+++ C Programming/if-else-statement.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//if else statement
+
+int main(int argc, char **argv)
+{
+
+int a = 2;
+
+if(a==1)
+{
+        printf("True\n");
+}
+
+else
+{
+        printf("False\n");
+}
+
+return 0;
+}
+
+
