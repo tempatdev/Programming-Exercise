c052d034324f9c85991ff123b753acd73519c33c
Homeless Ghetto Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-03 23:05:09 +0700

    Create switch-statement.c

---
+++ C Programming/switch-statement.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//switch statement
+
+int main(int argc, char **argv)
+{
+int a=1;
+
+switch(a)
+{
+        case 1:
+                {
+                        printf("The value is 1\n");
+                        break;
+                }
+        default:
+                {
+                        printf("Out of ****  \n");
+                        break;
+                }
+}
+
+return 0;
+}
+
