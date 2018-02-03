ade7de1e62f35b0f032386544d133af53f0ea7a1
Homeless Ghetto Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-03 22:57:42 +0700

    Create nested-if-statement.c

---
+++ C Programming/nested-if-statement.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//nested if statement
+
+int main(int argc, char **argv)
+{
+
+int a = 1, b = 2;
+
+if(a==1)
+{
+        if(b==2)
+        {
+                printf("True\n");
+        }
+}
+
+
+return 0;
+}
+
+
+
