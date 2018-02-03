cf4065296c62a93486deef4c2b970f25a8c281cf
Homeless Ghetto Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-03 23:46:05 +0700

    Create nested-loops.c

---
+++ C Programming/nested-loops.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Nested loop
+
+int main(int argc, int **argv)
+{
+int a,b=1,c=1,i;
+
+
+//While loop
+while(b<3)
+{
+
+        //Do While loop
+        do
+        {
+                //For loop
+                for(i=0;i<5;i++)
+                {
+                        printf("For looping #%d \n",i);
+                }
+
+                printf("Do While loop #%d \n", c);
+                c++;
+                
+        }while(c<2);
+
+        printf("While loop #%d \n",b);
+        b++;
+
+        
+}
+
+
+
+
+
+return 0;       
+}
