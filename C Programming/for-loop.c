086223e57641e643a93740f464cc3f8ace4c39ea
Homeless Ghetto Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-03 23:35:37 +0700

    Create for-loop.c

---
+++ C Programming/for-loop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//For loop
+
+int main(int argc, int **argv)
+{
+int a;
+
+
+for(a=0;a<3;a++)
+{
+        printf("For loop #%d \n",a);     //print the a variable value inside for loop
+}
+
+
+
+return 0;       
+}
+
+
