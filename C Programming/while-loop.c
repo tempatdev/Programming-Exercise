cae6585029a4f98dd7b72e958d25f3fd91b97e25
Homeless Ghetto Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-03 23:30:53 +0700

    Create while-loop.c

---
+++ C Programming/while-loop.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//While loop
+
+int main(int argc, int **argv)
+{
+int a=1;
+
+while(a<3)
+{
+        printf("This is while loop #%d \n",a);      //Print the 'a' variable value
+        a++;      // increament statement
+}
+
+
+
+return 0;       
+}
