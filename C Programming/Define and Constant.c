26f5a2bf3b68e1d32f00635e1d8484860050e6e4
Homeless Ghetto Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-03 16:23:38 +0700

    Create Define and Constant.c

---
+++ C Programming/Define and Constant.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define phi 3.14
+
+
+int main(int argc, char **argv)
+{
+const float phi2 = 3.14;
+int r=10;
+
+printf("Circle result with Define Preprocessor is %.2f \n",phi*r*r);
+printf("Circle result with Const Variable is %.2f \n", phi2*r*r);
+
+
+
+
+return 0;
+}
