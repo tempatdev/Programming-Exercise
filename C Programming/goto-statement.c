e8ab68c3c3348f01403764fd9ae317ca33b36ad3
Homeless Ghetto Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-03 23:58:58 +0700

    Create goto-statement.c

---
+++ C Programming/goto-statement.c
@@ -0,0 +1,21 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+//goto statement
+
+
+int main(int argc, char **argv)
+{
+
+startx:
+printf("goto statement \n");    
+goto startx;
+
+
+
+
+
+return 0;
+}
+
