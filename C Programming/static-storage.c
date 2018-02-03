d1626abb46ae732fd9a38ae91ce635e686391f58
Homeless Ghetto Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-03 16:46:52 +0700

    Create static-storage.c

---
+++ C Programming/static-storage.c
@@ -0,0 +1,18 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+//Static var
+static int a = 10;           
+
+
+
+int main(int argc, char **argv)
+{
+
+//Print the static var value    
+printf("Static var value is %d\n", a);  
+        
+
+return 0;
+}
