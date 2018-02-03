ca30557d91ee2bb052fef98d45b7f528856940a9
Assembled Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-04 01:22:58 +0700

    Create local-variable.c

---
+++ C Programming/local-variable.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Local Variable
+
+
+
+int main(int argc, char **argv)
+{
+//These are local variables
+int a,b,c;
+//end of local variables
+
+a=1,b=2,c=3;
+
+printf("Sum is %d\n",a+b+c);
+
+return 0;
+
+}
+
