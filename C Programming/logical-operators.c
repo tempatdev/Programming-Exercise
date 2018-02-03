58a6531362fa9a60eec17c3844c783474f2e1f46
Homeless Ghetto Sniper <35886850+tempatdev@users.noreply.github.com>
2018-02-03 17:29:41 +0700

    Create logical-operators.c

---
+++ C Programming/logical-operators.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+int a=1,b=2;
+
+// and "&&" logical operator
+if(a==1 && b==2)
+{
+        printf("Logical \"&&\" result is True\n");
+}
+
+// or "||" logical operator
+if(a==1 || b==0)
+{
+        printf("Logical \"||\" result is True\n");
+}
+
+// not "!" logical operator
+if(!(a==2))
+{
+        printf("Logical \"!\" result is True\n");
+}
+
+return 0;
+}
